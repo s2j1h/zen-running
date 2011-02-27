e6c9e37c1e5a65553db01e9692d408488efdb451
Julien Raigneau <julien@zeneffy.fr>
2011-02-27 22:53:31 +0100

    Commit with bundle

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 require 'sinatra'
  
 configure :production do
-require 'newrelic_rpm'
+	require 'newrelic_rpm'
 end
  
 # Quick test
 get '/' do
-"Hello from the ratpac
+	"Hello from the ratpac

6f340f1498b3d3f6668e59369323e56e55cefaf4
Julien Raigneau <julien@zeneffy.fr>
2011-05-14 12:25:37 +0200

    first public commit

--- Gemfile
+++ Gemfile
@@ -1,6 +1,17 @@
 source :gemcutter
 gem 'sinatra', '1.2.6'
-gem 'newrelic_rpm', '2.14.1'
 gem 'koala' , '1.0.0'
 gem 'haml', '3.1.1'
 gem 'rack-flash', '0.1.1'
+gem 'sinatra-redirect-with-flash', '0.1.2'
+gem 'data_mapper', '1.1.0'
+
+
+group :production do
+  gem 'dm-postgres-adapter', '1.1.0'
+  gem 'newrelic_rpm', '2.14.1'
+end
+
+group :development do
+  gem 'dm-sqlite-adapter', '1.1.0'
+end
--- app.rb
+++ app.rb
@@ -1,27 +1,56 @@
 # -*- coding: utf-8 -*-
 require 'rubygems'
+require 'data_mapper'
 require 'sinatra'
 require 'koala'
 require 'rack-flash'
+require 'sinatra/redirect_with_flash'
 require 'haml'
+
 
 enable :sessions
 
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
 configure :production do
 	require 'newrelic_rpm'
+   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
 set :root, File.dirname(__FILE__)
 set :sessions, true
+set :app_id, ENV['app_id'] # in dev mode: export app_id=xxx
+set :app_secret, ENV['app_secret'] # in dev mode: app_secret=xxx
+set :app_url, ENV['app_url'] ||  "http://localhost:4567"
 
-use Rack::Flash
+
+use Rack::Flash, :sweep => true
+
+
+class Hommage
+  include DataMapper::Resource  
+  property :id,                   Serial
+  property :id_user,              String
+  property :date,                 String
+  property :duree,                String
+  property :distance,             String
+  property :commentaires,         Text
+end
+
+
+DataMapper.auto_upgrade!
+#DataMapper.auto_migrate! #si changement de schéma à faire apparaître en production
+DataMapper::Model.raise_on_save_failure = false #permet de savoir si tout est bien sauvegardé, à utiliser avec rescue
+
 
 helpers do
 
   def oauth
-    app_id         =  "822050aa6886db8e663cbb1f9b1c63d3"
-    app_secret     =  "ab8aa063c069e6a6f5a10c2f5c5ccf13"
-    app_url        =  "http://localhost:4567"
+    app_id         =  settings.app_id
+    app_secret     =  settings.app_secret
+    app_url        =  settings.app_url
     
     callback_url = "#{ app_url }/oauth"
     @oauth ||= Koala::Facebook::OAuth.new app_id, app_secret, callback_url

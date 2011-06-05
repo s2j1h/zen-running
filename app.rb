4d94c07cf751320a6d933abf765cfce03edb74a5
Julien Raigneau <julien@zeneffy.fr>
2011-06-05 20:43:16 +0200

    added dirty hack for ssl issues

--- app.rb
+++ app.rb
@@ -8,6 +8,17 @@ require 'sinatra/redirect_with_flash'
 require 'haml'
 require 'date'
 
+
+class Net::HTTP
+  alias_method :old_initialize, :initialize
+  def initialize(*args)
+    old_initialize(*args)
+    @ssl_context = OpenSSL::SSL::SSLContext.new
+    @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end
+end
+
+
 enable :sessions
 
 
